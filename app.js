265504b8aeadd4b029bf5a17f873a2ea7f804b56
himanshu chauhan <46714243+himanshuchauhan234@users.noreply.github.com>
2023-08-10 22:30:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", (req, res) => {
-    res.render("index.ejs");
-})
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(port, () => {
     console.log("Started...")
